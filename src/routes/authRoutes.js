1fb355b12327a4f174ff237ad0cc37f70f42e19c
Nugroho-00 <snugroho211@gmail.com>
2021-06-11 01:05:21 +0700

    add new fix create pin , reset password otp

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,12 +3,21 @@ const {
   registerAccount,
   loginAccount,
   createPinUser,
+  postOTP,
+  verifyOTP,
+  resetPassword,
   logoutToken,
 } = require("../handlers/authHandlers");
+const { authentikasi } = require("../middlewares/authentication");
 
 Router.post("/register", registerAccount);
 Router.post("/login", loginAccount);
-Router.patch("/create/pin", createPinUser);
+Router.patch("/create-pin", authentikasi, createPinUser);
 Router.post("/logout", logoutToken);
+
+// reset password
+Router.post("/send-otp", postOTP);
+Router.post("/verify-otp", verifyOTP);
+Router.post("/reset-password", authentikasi, resetPassword);
 
 module.exports = Router;
