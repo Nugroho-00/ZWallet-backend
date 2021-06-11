e55ec6d616e0f915a0dbc869312d728eb1fb9ae0
Nugroho-00 <snugroho211@gmail.com>
2021-06-11 22:12:06 +0700

    fixing response error & fixing send otp and verify , add validation pin

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,18 +7,19 @@ const {
   verifyOTP,
   resetPassword,
   logoutToken,
+  validationPin,
 } = require("../handlers/authHandlers");
 const { authentication } = require("../middlewares/authentication");
 
 Router.post("/register", registerAccount);
 Router.post("/login", loginAccount);
-// Router.post("/validation-pin", authentication, validationPin);
-Router.patch("/create-pin", authentication, createPinUser);
+Router.post("/validation-pin", authentication, validationPin);
+Router.post("/create-pin", authentication, createPinUser);
 Router.post("/logout", logoutToken);
 
 // reset password
 Router.post("/send-otp", postOTP);
 Router.post("/verify-otp", verifyOTP);
-Router.post("/reset-password", authentication, resetPassword);
+Router.patch("/reset-password", authentication, resetPassword);
 
 module.exports = Router;
